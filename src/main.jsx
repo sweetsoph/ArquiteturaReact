2a80df711eafa7653610a947b84a695788c4d497
Sophia Verardo <sophia.verardods@gmail.com>
2025-12-07 16:15:53 -0300

    excluindo arquivos desnecessários e removendo suas menções

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <h1>Hello World!</h1>
   </StrictMode>,
 )
