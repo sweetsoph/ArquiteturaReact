2d15031bba93221cc5a7a43902db1ecdd0e1c6e6
Sophia Verardo <sophia.verardods@gmail.com>
2025-12-07 21:05:08 -0300

    utilizando biblioteca para alterar o title da página a cada página acessada

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,16 @@
 import { Container } from "../../assets/styles/style"
 import Link from "../../components/Buttons/Link"
+import { Helmet } from "react-helmet"
 
 function Home(){
-    return <Container><h1>Home Page</h1><Link texto="testando" /></Container>
+    return <>
+        <Helmet>
+            <title>Teste</title>
+        </Helmet>
+        <Container>
+            <h1>Home Page</h1><Link texto="testando" />
+        </Container>
+    </>
 }
 
 export default Home
